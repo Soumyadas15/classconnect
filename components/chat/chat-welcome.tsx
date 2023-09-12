4f1f0d067031156bcaa678e8f106b27577ddbe1f
Saumyapratim Das <saumyadas2017@gmail.com>
2023-09-12 14:14:33 +0530

    User icon

--- components/chat/chat-welcome.tsx
+++ components/chat/chat-welcome.tsx
@@ -1,5 +1,6 @@
-import { MessageCircle, Mic, ShieldAlert, ShieldCheck, Video, PenSquare, Book } from "lucide-react";
+import { MessageCircle, User2, ShieldAlert, ShieldCheck, Video, PenSquare, Book } from "lucide-react";
 import { ChannelType } from "@prisma/client";
+import { getOrCreateConversation } from "@/lib/conversation";
 
 interface ChatWelcomeProps {
   name: string;
@@ -11,12 +12,16 @@ export const ChatWelcome = ({
   type
 }: ChatWelcomeProps) => {
   
-  
   return (
     <div className="space-y-2 px-4 mb-4">
       {type === "channel" && (
         <div className="h-[75px] w-[75px] rounded-full bg-zinc-500 dark:bg-zinc-700 flex items-center justify-center">
           <MessageCircle className="h-12 w-12 text-white" />
+        </div>
+      )}
+      {type === "conversation" && (
+        <div className="h-[75px] w-[75px] rounded-full bg-zinc-500 dark:bg-zinc-700 flex items-center justify-center">
+          <User2 className="h-12 w-12 text-white" />
         </div>
       )}
       <p className="text-xl md:text-3xl font-bold">
